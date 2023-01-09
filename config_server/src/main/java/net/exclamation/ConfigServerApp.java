473cb4d44c847c0938043b51be98d58b626b7d41
Andrei Kalinichenko <andypanda197907@gmail.com>
2023-01-09 13:15:10 +0300

    Добавил для config-client файлы конфигурации. Изменил ConfigServerApp

--- config_server/src/main/java/net/exclamation/ConfigServerApp.java
+++ config_server/src/main/java/net/exclamation/ConfigServerApp.java
@@ -1,13 +1,23 @@
 package net.exclamation;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        SpringApplication.run(ConfigServerApp.class, args);
     }
 }
