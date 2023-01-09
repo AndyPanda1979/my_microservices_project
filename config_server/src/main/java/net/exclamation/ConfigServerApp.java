610d0f1ba0ce1d836b4865594b26c2dd1cbe567a
Andrei Kalinichenko <andypanda197907@gmail.com>
2023-01-09 12:39:10 +0300

    - Начинаем работу по созданию сервера конфигураций

---
+++ config_server/src/main/java/net/exclamation/ConfigServerApp.java
@@ -0,0 +1,13 @@
+package net.exclamation;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConfigServerApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
