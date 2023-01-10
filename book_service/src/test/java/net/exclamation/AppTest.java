22a75562458d8820f95b0765e90649a2fecba736
Andrei Kalinichenko <andypanda197907@gmail.com>
2023-01-11 00:36:53 +0300

    + добавил комментарии по mongoDB, протестировал Docker

--- book_service/src/test/java/net/exclamation/AppTest.java
+++ book_service/src/test/java/net/exclamation/AppTest.java
@@ -3,12 +3,19 @@ package net.exclamation;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
+
+    @BeforeAll
+    public static void initData() {
+        System.out.println("Initialization...");
+    }
+
     /**
      * Rigorous Test :-)
      */
